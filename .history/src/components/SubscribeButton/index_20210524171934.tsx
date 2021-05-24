3acb237a142793c7c616ae5d21ad672794a29508
rafael346 <sousa_raf@hotmail.com>
2021-05-24 17:43:29 -0300

    Componentes adicionados, SSG, metodo de pagamento

---
+++ .history/src/components/SubscribeButton/index_20210524171934.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss'
+
+interface SubscribeButtonProps {
+  priceId: number;
+}
+
+export function SubscribeButton ({ priceId }: SubscribeButtonProps){
+  return(
+    <button
+      type="button"
+      className={styles.subscribeButton}
+    >
+      Subscribe Now
+    </button>
+  );
+}
